24feb29f8890bd7bc8d0bd0ede97e92a89a5785e
worldmars <474698445@163.com>
2018-06-25 19:17:05 +0800

    add vendor script (#3)

---
+++ vendor.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+display_help()
+{
+    echo
+    echo "usage: $0 -u url"
+    echo "option:"
+    echo "-u url(github)"
+    echo
+    echo
+    exit 0
+}
+
+CUR_PATH=$(cd `dirname $0`; pwd)
+# parse options usage: $0 -c config.ini -m flag -b base_path -r remote_path -u user_name -p pwd -t
+while getopts 'u:' OPT; do
+    case $OPT in
+        u)
+            git_url="$OPTARG";;
+        ?)
+            display_help
+    esac
+done
+
+if [ ! -n "$git_url" ]; then
+    echo "url must be set up"
+    exit 0
+fi
+
+exist=`which govendor | wc -l`
+if [ $exist == 0 ]; then
+    echo "please install govendor"
+    go get -u github.com/kardianos/govendor
+fi
+govendor list -v list
+govendor fetch $git_url
+govendor fmt +local
+govendor install +local
+govendor list -v list
